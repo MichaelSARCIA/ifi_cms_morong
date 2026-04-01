7cbef59368dd3823159e0fa3373631b0ff6054b0
Michael SARCIA <michaelsarcia@users.noreply.github.com>
2026-04-01 09:16:11 +0800

    Fix: Added GET logout route to prevent '419 Page Expired' errors on session timeout.

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::get('/forgot-password', [AuthController::class, 'forgotPassword'])->name('password.request');
 Route::post('/forgot-password', [AuthController::class, 'sendResetLink'])->name('password.email');
